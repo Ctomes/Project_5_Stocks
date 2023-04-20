57d6fb5f00ed82d7d57a6d777d95ca58328c31b9
Ctomes <72369162+Ctomes@users.noreply.github.com>
2023-04-19 20:07:35 -0700

    Initial commit

---
+++ stocks_proj.py
@@ -0,0 +1,18 @@
+#Tomes, Christopher
+#Cal Poly Pomona CS4650
+#stocks_proj.py
+#This program will make a prediction on which stocks to buy and sell.
+#
+
+print('This should be checking the SMP500')
+
+#Data mine some values.
+#Avoid large block ownership.
+#Tesla is an example of the oppisite.
+#There is a BID-ASK-SPREAD
+#Im thinking tech sector. Gigabyte, Intel, AMD, NVidia, EVGA, ASUS, SAMSUNG, LG etc
+print('Downloading Dat...')
+
+print('Training Data..')
+
+print('Make Prediction:')
